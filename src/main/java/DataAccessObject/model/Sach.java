bf057442b302d97595ecf413493ce30d8fd08b9d
Yuh <huypham3188@gmail.com>
2024-06-23 22:41:07 +0700

    update all project

---
+++ src/main/java/DataAccessObject/model/Sach.java
@@ -0,0 +1,57 @@
+package DataAccessObject.model;
+
+public class Sach {
+	private String id;
+	private String tenSach;
+	private Float giaBan;
+	private int namXuatBan;
+
+	public Sach() {
+
+	}
+
+	public Sach(String id, String tenSach, Float giaBan, int namXuatBan) {
+		this.setId(id);
+		this.setTenSach(tenSach);
+		this.setGiaBan(giaBan);
+		this.setNamXuatBan(namXuatBan);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTenSach() {
+		return tenSach;
+	}
+
+	public void setTenSach(String tenSach) {
+		this.tenSach = tenSach;
+	}
+
+	public Float getGiaBan() {
+		return giaBan;
+	}
+
+	public void setGiaBan(Float giaBan) {
+		this.giaBan = giaBan;
+	}
+
+	public int getNamXuatBan() {
+		return namXuatBan;
+	}
+
+	public void setNamXuatBan(int namXuatBan) {
+		this.namXuatBan = namXuatBan;
+	}
+
+	@Override
+	public String toString() {
+		return "Sach [id=" + id + ", tenSach=" + tenSach + ", giaBan=" + giaBan + ", namXuatBan=" + namXuatBan + "]";
+	}
+	
+}
---
+++ src/main/java/DataAccessObject_PreparedStatement/test/TestKhachHang.java
@@ -0,0 +1,47 @@
+package DataAccessObject_PreparedStatement.test;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+import DataAccessObject_PreparedStatement.dao.KhachHangDAO;
+import DataAccessObject_PreparedStatement.dao.SachDAO;
+import DataAccessObject_PreparedStatement.model.KhachHang;
+import DataAccessObject_PreparedStatement.model.Sach;
+
+public class TestKhachHang {
+	public static void main(String[] args) {
+//		Date ns1 = Date.valueOf("1999-12-1");
+//		KhachHang kh1 = new KhachHang(1, "Huy", ns1, "TPHoChiMinh");
+//		Date ns2 = Date.valueOf("2002-2-2");
+//		KhachHang kh2 = new KhachHang(2, "Gia", ns2, "TPHaNoi");
+//		Test insert
+//		KhachHangDAO.getInstance().insert(kh1);
+//		
+//		Test update
+//		Date ns1 = Date.valueOf("1990-12-1");
+//		KhachHang kh1 = new KhachHang(1, "GiaHuy", ns1, "TPHoChiMinh");
+//		KhachHangDAO.getInstance().update(kh1);
+//		
+//		Test delete
+//		KhachHang kh = new KhachHang(1, "", null, "");
+//		KhachHangDAO.getInstance().delete(kh);
+//		
+//		Test selectall
+//		ArrayList<KhachHang> list = KhachHangDAO.getInstance().selectAll();
+//		for(KhachHang k : list) {
+//			System.out.println(k.toString());
+//		}
+		
+//		Test selectbyid
+//		KhachHang find = new KhachHang();
+//		find.setId(1);
+//		KhachHang k = KhachHangDAO.getInstance().selectById(find);
+//		System.out.println(k.toString());
+		
+//		Test selectbycondition
+		ArrayList<KhachHang> list = KhachHangDAO.getInstance().selectByCondition("DiaChi LIKE \"%C%\"");
+		for(KhachHang k : list) {
+			System.out.println(k.toString());
+		}
+	}
+}
